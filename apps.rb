a7562df8a4b0705be36648c638abe7c6b30d4229
Max Wheeler <max@makenosound.com>
2014-10-24 13:52:42 +1100

    Typo.

--- apps.rb
+++ apps.rb
@@ -1,4 +1,4 @@
-def cask(name); dep name, :template => "icelab:cask"; end # Avoid repitition below
+def cask(name); dep name, :template => "icelab:cask"; end # Avoid repetition below
 
 cask "caffeine"
 cask "charles"
